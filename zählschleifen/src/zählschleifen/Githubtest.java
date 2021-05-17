551e817a2b479d273daa306ff9d8264e606aea12
FSMT1_7 <FSMT1_7@UG101_07.domino1.local>
2021-05-17 11:13:33 +0200

    Textupdate

--- zählschleifen/src/zählschleifen/Githubtest.java
+++ zählschleifen/src/zählschleifen/Githubtest.java
@@ -1,10 +1,15 @@
 package zählschleifen;
 
+import java.util.Scanner;
+
 public class Githubtest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner input=new Scanner(System.in);
+		
+		System.out.println("Du machst auch alles für Werkzeug ;)");
 	}
 
 }
